19ecfaad3683b5128757ba20452acc81aa7eb876
legendstrange <legendstrange1992@gmail.com>
2019-03-18 23:20:08 +0700

    A

--- xuly/dathang.php
+++ xuly/dathang.php
@@ -14,6 +14,19 @@ foreach($cart as $k => $v){
 }
 $sql = "insert into dondathang(tenkhachhang,sodienthoai,email,diachi,ngaydathang,tongtien) values ('$fullname',$phone,'$email','$address','$ngaydathang',$tongtien)";
 mysqli_query($ketnoi,$sql);
+$top_donhang = mysqli_query($ketnoi,"select * from dondathang order by ma_donhang desc limit 1");
+$row_top_donhang = mysqli_fetch_assoc($top_donhang);
+$ma_donhang_top = $row_top_donhang['ma_donhang'];
+foreach($cart as $k => $v){
+    $MADT = $v['id_sanpham'];
+    $TENDT = $v['tensanpham'];
+    $hinh = $v['hinh'];
+    $soluong = $v['soluong'];
+    $gia = $v['gia'];
+    $thanhtien = $v['thanhtien'];
+    $sql2 = "insert into chitietdonhang(ma_donhang,MADT,TENDT,hinh,soluong,gia,thanhtien) values ($ma_donhang_top,$MADT,'$TENDT','$hinh',$soluong,$gia,$thanhtien)";
+    mysqli_query($ketnoi,$sql2);
+}
 echo "success";
 
 ?>
